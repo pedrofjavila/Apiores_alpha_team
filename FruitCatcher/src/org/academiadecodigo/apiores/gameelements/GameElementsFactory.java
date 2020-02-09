34d9e359dcd330713e6547b24584ff932652c79e
Gustavo <gustavo.mendes@live.com.pt>
2020-02-09 16:22:23 -0100

    changed factory constructors

--- FruitCatcher/src/org/academiadecodigo/apiores/gameelements/GameElementsFactory.java
+++ FruitCatcher/src/org/academiadecodigo/apiores/gameelements/GameElementsFactory.java
@@ -1,57 +1,63 @@
 package org.academiadecodigo.apiores.gameelements;
 
+import org.academiadecodigo.apiores.gameelements.grid.GridPosition;
 import org.academiadecodigo.apiores.gameelements.objects.*;
 import org.academiadecodigo.apiores.gameelements.players.*;
+import org.academiadecodigo.apiores.simplegfx.SimpleGfxGrid;
 
 public class GameElementsFactory {
 
-    public static GameObject createNewGameObject(){
+    public static GameObject createNewGameObject(SimpleGfxGrid grid){
 
         int randomObject = (int) (Math.random() * ObjectType.values().length);
         ObjectType objectType = ObjectType.values()[randomObject];
+        int randomCol = (int) (Math.random() * grid.getCols());
+        
         GameObject object = null;
 
         switch(objectType) {
             case BEER:
-                object = new Beer();
+                object = new Beer(grid.makeGridPosition(randomCol, 0));
                 break;
             case CAR:
-                object = new Car();
+                object = new Car(grid.makeGridPosition(randomCol, 0));
                 break;
             case BRACKETS:
-                object = new Brackets();
+                object = new Brackets(grid.makeGridPosition(randomCol, 0));
                 break;
             case PINEAPPLE:
-                object = new Pineapple();
+                object = new Pineapple(grid.makeGridPosition(randomCol, 0));
                 break;
             case SUMARIZER:
-                object = new Sumarizer();
+                object = new Sumarizer(grid.makeGridPosition(randomCol, 0));
                 break;
             case BAD_DESIGN:
-                object = new BadDesign();
+                object = new BadDesign(grid.makeGridPosition(randomCol, 0));
                 break;
         }
         return object;
     }
 
-    public static Player createNewPlayer(){
+    public static Player createNewPlayer(SimpleGfxGrid grid){
 
         int randomPlayer = (int) (Math.random() * PlayerType.values().length);
         PlayerType playerType = PlayerType.values()[randomPlayer];
+        int randomCol = (int) (Math.random() * grid.getCols());
+
         Player player = null;
 
         switch (playerType) {
             case JOJO:
-                player = new Jojo();
+                player = new Jojo(grid.makeGridPosition();
                 break;
             case RICARDO:
-                player = new Ricardo();
+                player = new Ricardo(grid.makeGridPosition();
                 break;
             case SORAIA:
-                player = new Soraia();
+                player = new Soraia(grid.makeGridPosition();
                 break;
             case RITA:
-                player = new Rita();
+                player = new Rita(grid.makeGridPosition();
                 break;
         }
         return player;
