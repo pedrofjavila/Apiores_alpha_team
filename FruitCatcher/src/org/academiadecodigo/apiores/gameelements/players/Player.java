2e006ac2acfa3b29eb4fbd5b4e015708302ee72f
pedroavila <pedrofjavila@gmail.com>
2020-02-09 16:39:02 -0100

    added player

--- FruitCatcher/src/org/academiadecodigo/apiores/gameelements/players/Player.java
+++ FruitCatcher/src/org/academiadecodigo/apiores/gameelements/players/Player.java
@@ -1,4 +1,39 @@
 package org.academiadecodigo.apiores.gameelements.players;
 
-public class Player {
+import org.academiadecodigo.simplegraphics.keyboard.Keyboard;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardHandler;
+import org.academiadecodigo.simplegraphics.pictures.Picture;
+
+public abstract class Player implements KeyboardHandler {
+
+    private boolean alive = true;
+    private Keyboard keyboard;
+    private int score;
+    private Picture picture;
+
+
+    public boolean isAlive(){
+        return alive;
+    }
+
+    public void setAlive(boolean alive) {
+        this.alive = alive;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public void setPicture(Picture picture){
+
+    }
+
+
 }
+
+
+
